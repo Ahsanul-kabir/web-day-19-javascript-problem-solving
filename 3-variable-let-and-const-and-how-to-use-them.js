23ecd4023563ced855644f5fe2b5aab510479c88
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-19 21:26:25 +0600

    3-variable-let-and-const-and-how-to-use-them

---
+++ 3-variable-let-and-const-and-how-to-use-them.js
@@ -0,0 +1,13 @@
+var name = 'Habibur';
+if (name.length > 4) {
+    name = 'Habu';
+}
+console.log(name);
+
+let age = 16; // let means variable is changeable
+age = 16;
+age = 17;
+console.log(age);
+
+const country = 'Bangladesh' // const = constant means variable is not changeabe
+console.log(country);
