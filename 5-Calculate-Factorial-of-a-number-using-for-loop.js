c42d023e7b6282404acf8cf2e65138877a226128
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-20 11:47:55 +0600

    5-Calculate-Factorial-of-a-number-using-for-loop updated

--- 5-Calculate-Factorial-of-a-number-using-for-loop.js
+++ 5-Calculate-Factorial-of-a-number-using-for-loop.js
@@ -1,3 +1,4 @@
+/* general way
 let num = 5;
 
 let fact = 1;
@@ -5,4 +6,15 @@ let fact = 1;
 for (let i = 1; i <= num; i++) {
     fact = fact * i;
 }
-console.log(fact);
+console.log(fact); */
+
+function factorial(num) {
+    let fact = 1;
+
+    for (let i = 1; i <= num; i++) {
+        fact = fact * i;
+    }
+    return fact;
+}
+let result = factorial(5);
+console.log(result);
