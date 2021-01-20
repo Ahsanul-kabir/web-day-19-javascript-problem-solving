1ba84cbef1e79c6b76b1a9bc71b938148f24e274
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-20 12:03:00 +0600

    6-Calculate-Factorial-of-a-number-using-a-while-loop added

--- 6-Calculate-Factorial-of-a-number-using-a-while-loop.js
+++ 6-Calculate-Factorial-of-a-number-using-a-while-loop.js
@@ -1,3 +1,4 @@
+/* General Way
 var i = 1,
     num = 5,
     fact = 1;
@@ -6,4 +7,18 @@ while (i <= num) {
     fact = fact * i;
     i++;
 }
-console.log(fact);
+console.log(fact); */
+
+function fact(n) {
+    var i = 1,
+        fact = 1;
+
+    while (i <= n) {
+        fact = fact * i;
+        i++;
+    }
+    return fact;
+}
+
+var result = fact(5);
+console.log(result);
