7d52e1d223a109eeef51a1d237639ab18fe521f0
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-20 20:02:21 +0600

    10-Create-Fibonacci-series-in-a-recursive-way added

---
+++ 10-Create-Fibonacci-series-in-a-recursive-way.js
@@ -0,0 +1,14 @@
+function fibonacciNum(n) {
+    if (n == 0) {
+        return [0];
+    } else if (n == 1) {
+        return [0, 1];
+    } else {
+        var fibo = fibonacciNum(n - 1);
+        var nextElement = fibo[n - 1] + fibo[n - 2];
+        fibo.push(nextElement);
+        return fibo;
+    }
+}
+var result = fibonacciNum(10);
+console.log(result);
